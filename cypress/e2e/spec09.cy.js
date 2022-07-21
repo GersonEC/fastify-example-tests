40e3e3541db531ed7588f5e02611fd9f1536ccb5
GersonEC <gersonenriquez94@gmail.com>
2022-07-21 08:19:43 +0200

    Implement until spec 10

--- cypress/e2e/spec09.cy.js
+++ cypress/e2e/spec09.cy.js
@@ -4,10 +4,63 @@ it('returns different fruits', () => {
   // stub the /fruit endpoint to return "apple" on the first visit
   // stub the /fruit endpoint to return "grapes" on the second visit
   // https://on.cypress.io/intercept with "times: *" option
-  //
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    {
+      fruit: 'grapes',
+    },
+  )
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    {
+      fruit: 'apple',
+    },
+  )
   // visit the site
-  //
+  cy.visit('/')
   // confirm it shows "apple"
+  cy.contains('#fruit', 'apple').should('be.visible')
+
   // reload the site
+  cy.reload()
   // confirm it shows "grapes"
+  cy.contains('#fruit', 'grapes').should('be.visible')
+})
+
+it('returns different fruits', () => {
+  // stub the /fruit endpoint to return "apple" on the first visit
+  // stub the /fruit endpoint to return "grapes" on the second visit
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    { fruit: 'grapes' },
+  )
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    { fruit: 'apple' },
+  )
+
+  // visit the site
+  cy.visit('/')
+  // confirm it shows "apple"
+  cy.contains('#fruit', 'apple').should('be.visible')
+  // reload the site
+  cy.reload()
+  // confirm it shows "grapes"
+  cy.contains('#fruit', 'grapes').should('be.visible')
 })
--- cypress/e2e/spec10.cy.js
+++ cypress/e2e/spec10.cy.js
@@ -6,8 +6,51 @@ it('shows the loading element with Promise.delay', () => {
   // Cypress.Promise.delay(2000) and the response function
   // hint: see https://on.cypress.io/intercept and
   // the response callback function
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+    },
+    (req) => {
+      return Cypress.Promise.delay(2000)
+    },
+  ).as('fruit-delayed')
+
   // visit the site
+  cy.visit('/')
+
   // make sure the loading element is visible
+  cy.contains('#fruit', 'loading...').should('be.visible')
+
   // from the request spy, get the response body
   // and confirm the fruit returned by the server is visible
+  cy.wait('@fruit-delayed')
+    .its('response.body.fruit')
+    .then(cy.log)
+    .then((fruit) => {
+      cy.contains('#fruit', fruit).should('be.visible')
+    })
+})
+
+it('shows the loading element with Promise.delay', () => {
+  // set up an intercept to spy on the GET /fruit endpoint
+  // but delay the request by 2 seconds using
+  // Cypress.Promise.delay(200) and the response function
+  // hint: see https://on.cypress.io/intercept and
+  // the response callback function
+  cy.intercept('GET', '/fruit', (req) => {
+    return Cypress.Promise.delay(2000)
+  }).as('fruit')
+  // visit the site
+  cy.visit('/')
+  // make sure the loading element is visible
+  cy.contains('#fruit', 'loading...').should('be.visible')
+  // from the request spy, get the response body
+  // and confirm the fruit returned by the server is visible
+  cy.wait('@fruit')
+    .its('response.body.fruit')
+    .then(cy.log)
+    .then((fruit) => {
+      cy.contains('#fruit', fruit).should('be.visible')
+    })
 })
