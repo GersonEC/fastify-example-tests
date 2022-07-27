7cf1aa7d59bd41029f377a389430511ed98d8834
GersonEC <gersonenriquez94@gmail.com>
2022-07-27 11:17:58 +0200

    Implement until spec 18

--- cypress/e2e/spec16.cy.js
+++ cypress/e2e/spec16.cy.js
@@ -1,11 +1,20 @@
 /// <reference types="cypress" />
 
 it('requests all fruits', () => {
-  // request the fruit from the /fruit endpoint
-  // using the https://on.cypress.io/request command
-  // from the response get the body object, then the fruit
-  // using the https://on.cypress.io/its command
-  // store each fruit in a Set object
-  // and keep requesting until we see a fruit already in the set
-  // print the collected list of fruits
+  const fruits = new Set()
+
+  function getTheFruit() {
+    cy.request('GET', '/fruit')
+      .its('body.fruit')
+      .then((fruit) => {
+        if (fruits.has(fruit)) {
+          const list = [...fruits].sort()
+          cy.log(list.join(', '))
+        } else {
+          fruits.add(fruit)
+          cy.wait(500).then(getTheFruit)
+        }
+      })
+  }
+  getTheFruit()
 })
--- cypress/e2e/spec18.cy.js
+++ cypress/e2e/spec18.cy.js
@@ -6,19 +6,34 @@ it('does not intercept the requests made using cy.request', () => {
   // set up the intercept and make the cy.request call
   cy.intercept('GET', '/fruit', cy.spy().as('getFruit'))
   // Confirm the cy.intercept does not "see" cy.request network calls
+  cy.request('/fruit')
+    .its('body')
+    .should('have.property', 'fruit')
+    .and('be.oneOf', [
+      'Apples',
+      'Bananas',
+      'Grapes',
+      'Oranges',
+      'Pears',
+    ])
+
+  cy.get('@getFruit').should('not.be.called')
 })
 
 it('catches the request made by the test using fetch', () => {
   // The test itself can use the "fetch" to make network calls
   // Use "fetch" instead ot request to make a network call
   // and confirm that cy.intercept does "see" the network call
-  //
-  // set up the intercept
-  // make the fetch call and confirm the response body
-  // has the property "fruit"
-  // check that the intercept has worked using cy.wait
-  //
-  // tip 1: make sure to make the "fetch" call AFTER the cy.intercept
+  cy.intercept('GET', '/fruit')
+    .as('getFruit')
+    .then(() => {
+      fetch('/fruit')
+        .then((r) => r.json())
+        .then((body) => {
+          expect(body).to.have.property('fruit')
+        })
+    })
+  cy.wait('@getFruit').its('response.body')
 })
 
 it('waits for the fetch to finish using cy.wrap command', () => {
@@ -28,6 +43,13 @@ it('waits for the fetch to finish using cy.wrap command', () => {
   // around the promise returned by the "fetch"
   // from the response, get the json by invoking the "json" method
   // confirm the body has the property "fruit"
+
+  cy.intercept('GET', '/fruit').as('fruit')
+  const fruitFetchPromise = window.fetch('/fruit')
+
+  cy.wrap(fruitFetchPromise)
+    .invoke('json')
+    .should('have.property', 'fruit')
 })
 
 it('invokes window.fetch using the cy.invoke command', () => {
@@ -39,4 +61,13 @@ it('invokes window.fetch using the cy.invoke command', () => {
   // invoke the method "json" on the response
   // and confirm the body has the property "fruit"
   // from the intercept get the fruit and confirm the "fetch" response body
+  cy.intercept('GET', '/fruit').as('getFruit')
+  cy.window()
+    .invoke('fetch', '/fruit')
+    .invoke('json')
+    .then((body) => {
+      cy.get('@getFruit')
+        .its('response.body')
+        .should('deep.equal', body)
+    })
 })
