d91ec6948d5f3a11a68461c96b1a94b9b4c876ee
GersonEC <gersonenriquez94@gmail.com>
2022-07-28 10:30:19 +0200

    Implement until spec 21

--- cypress/e2e/spec20.cy.js
+++ cypress/e2e/spec20.cy.js
@@ -4,4 +4,11 @@ it('modifies the HTML responses', () => {
   // intercept the HTML document "/"
   // and change the response body by replacing a word
   // confirm the new word is shown on the page
+  cy.intercept('GET', '/', (req) => {
+    req.continue((res) => {
+      res.body = res.body.replace('random', 'ciao')
+    })
+  })
+  cy.visit('/')
+  cy.contains('ciao')
 })
