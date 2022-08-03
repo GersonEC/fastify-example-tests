faf4a9e4cdb56ac14d9f341bcbf50928682906fd
GersonEC <gersonenriquez94@gmail.com>
2022-08-03 17:09:51 +0200

    Implement until 28

--- cypress/e2e/spec26.cy.js
+++ cypress/e2e/spec26.cy.js
@@ -3,21 +3,31 @@
 it('makes multiple separate requests', () => {
   // make the first request to fetch the fruit
   // using https://on.cypress.io/request
-  // make the second request to fetch the fruit again
-  // and compare the two responses - they should be different
-  // tip: use the ".then(callback)" to have both responses
-  // https://on.cypress.io/then command
-  // it will look like a pyramid of Doom of callbacks
+  cy.request('/fruit').then((res) => {
+    const fruit = res.body.fruit
+    cy.request('/fruit').then((res) => {
+      const fruit2 = res.body.fruit
+      expect(fruit).to.not.deep.equal(fruit2)
+    })
+  })
 })
 
 it('store multiple responses as aliases', () => {
   // make the first request using https://on.cypress.io/request
   // and store its "body.fruit" value as alias "fruit1"
   // https://on.cypress.io/its and https://on.cypress.io/as
+  cy.request('/fruit').its('body.fruit').as('fruit1')
+
   // make the second request and store the fruit as alias "fruit2"
   // use https://on.cypress.io/then command
   // with a "function () { ... }" callback
   // to be able to access both fruits using
   // "this.fruit1" and "this.fruit2" variables
   // and confirm they are different
+  cy.request('/fruit')
+    .its('body.fruit')
+    .as('fruit2')
+    .then(function () {
+      expect(this.fruit1).to.not.deep.equal(this.fruit2)
+    })
 })
